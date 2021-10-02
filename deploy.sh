aa87a5ca62425813faa1137816becfb98e55eda1
parth <parth@ormae.com>
2021-10-03 00:46:45 +0530

    fix

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+docker build -t kdsofts/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t  kdsofts/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t  kdsofts/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push kdsofts/multi-client:$SHA
+docker push kdsofts/multi-server:$SHA
+docker push kdsofts/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=kdsofts/multi-server:$SHA
+kubectl set image deployments/client-deployment client=kdsofts/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=kdsofts/multi-worker:$SHA
